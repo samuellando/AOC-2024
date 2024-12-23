e7fe6f150f51b8211105247d854a0d11a9805050
Samuel Lando <samuel.lando@aol.com>
2024-12-22 21:21:48 -0500

    Add tests for 16,17,18

--- 17/main_test.go
+++ 17/main_test.go
@@ -5,13 +5,13 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-    if Part1() != "0" {
+    if Part1() != "7,5,4,3,4,5,3,4,6" {
         t.Fail()
     }
 }
 
 func TestPart2(t *testing.T) {
-    if Part2() != 0 {
+    if Part2() != 164278899142333 {
         t.Fail()
     }
 }
